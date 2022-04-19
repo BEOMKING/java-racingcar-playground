1a2b882562d4db2df38fd5ab22704115f9e9783e
박범진 <qjawlsqjacks@fasoo.com>
2022-04-19 10:46:21 +0900

    배열 숫자 합 계산 메소드 추출

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,15 +3,23 @@ package calculator;
 public class Calculator {
 
 	public Integer splitAndSum(String input) {
-		if (input == null || input.isEmpty()) {
+		if (isEmptyOrNull(input)) {
 			return 0;
 		}
 		String[] numbers = input.split(",");
+		return calSum(numbers);
+	}
+
+	private int calSum(String[] numbers) {
 		int sum = 0;
 		for (int i = 0; i < numbers.length; i++) {
 			sum += stringToInt(numbers[i]);
 		}
 		return sum;
+	}
+
+	private boolean isEmptyOrNull(String input) {
+		return input == null || input.isEmpty();
 	}
 
 	public Integer stringToInt(String input) {
