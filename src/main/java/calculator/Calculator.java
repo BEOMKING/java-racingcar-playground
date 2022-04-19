0b512db4acd2d43c27f110c953f9d626789057a3
박범진 <qjawlsqjacks@fasoo.com>
2022-04-19 09:48:03 +0900

    초기화

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,15 +1,7 @@
 package calculator;
 
-import java.util.List;
-
 public class Calculator {
 
-	public static Integer add(List<Integer> numbers) {
-		int num = 0;
-		for (int i = 0; i < numbers.size(); i++) {
-			num += numbers.get(i);
-		}
-		return num;
-	}
+
 
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,44 +10,6 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 	
-	Validation valid;
-	
-	@BeforeEach
-	void setUp() {
-		valid = new Validation();
-	}
 
-	@Test
-	void 값_더하기() {
-		assertThat(Calculator.add(Arrays.asList(3, 2, 1))).isEqualTo(6);
-		assertThat(Calculator.add(Arrays.asList(3, 1333, 1))).isEqualTo(1337);
-	}
-
-	@Test
-	void 빈_입력값_처리() {
-		assertThat(valid.isValid(null)).isEqualTo(0);
-		assertThat(valid.isValid("")).isEqualTo(0);
-	}
-	
-	@Test
-	void 문자_숫자로_변환() {
-		assertThat(valid.isValid(null)).isEqualTo(0);
-		assertThat(valid.isValid("")).isEqualTo(0);
-		assertThat(valid.isValid("3")).isEqualTo(3);
-	}
-	
-	@Test
-	void 문자를_컴마로_구분() {
-		assertThat(valid.split("3,2,1")[0]).isEqualTo("3");
-		assertThat(valid.split("3,2,1")[1]).isEqualTo("2");
-		assertThat(valid.split("3,2,1")[2]).isEqualTo("1");
-	}
-	
-	@Test
-	void 분리한_문자를_숫자_리스트로_변환() {
-		assertThat(valid.stringToList(valid.split("3,2,1")).get(0)).isEqualTo(3);
-		assertThat(valid.stringToList(valid.split("3,2,1")).get(1)).isEqualTo(2);
-		assertThat(valid.stringToList(valid.split("3,2,1")).get(2)).isEqualTo(1);
-	}
 	
 }
