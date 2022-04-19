4e818ae46754b27f69ebb83be555b8cd4bc99dc1
박범진 <qjawlsqjacks@fasoo.com>
2022-04-19 09:58:07 +0900

    빈 문자열, null 값처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,6 +2,13 @@ package calculator;
 
 public class Calculator {
 
+	public static Integer splitAndSum(String input) {
+		if (input == null || input.isEmpty()) {
+			return 0;
+		}
+		return 0;
+	}
+
 
 
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
 	
-
+	@Test
+	void 빈_문자열_널값처리() {
+		assertThat(Calculator.splitAndSum(" ")).isEqualTo(0);
+		assertThat(Calculator.splitAndSum(null)).isEqualTo(0);
+	}
 	
 }
